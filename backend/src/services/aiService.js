a0bbdc0af119d992230098e5508a62c51a2dc100
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-12-10 09:39:18 -0300

    Create aiService.js

---
+++ backend/src/services/aiService.js
@@ -0,0 +1,9 @@
+async function generateAdvice() {
+  return (
+    'Dica geral: sempre que possível, prefira meios de transporte com menor emissão de CO₂, ' +
+    'como transporte coletivo, bicicleta ou caminhada. Considere também compensar suas emissões ' +
+    'por meio de projetos ambientais e reduzir viagens desnecessárias.'
+  );
+}
+
+module.exports = { generateAdvice };
